d8c9ff726d4017675f1f103478d8c623f5622b30
Andre Albuquerque <alalbuquerque22@gmail.com>
2020-10-30 10:16:23 -0300

    style organize and create api service

--- mobile/src/pages/ShowItems/ShowItems.tsx
+++ mobile/src/pages/ShowItems/ShowItems.tsx
@@ -1,6 +1,5 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import React, { useEffect, useState } from 'react'
-import {  View,Text} from 'react-native'
 
 import Header from      '../../components/Header'
 import {ContainerView} from '../../components/Container'
